b4407ea5461a733165925c9930e26eb09d62fe66
videsh <videsh.ghodarop@iaugmentor.com>
2020-07-18 09:04:36 +0530

    deployment || completed

---
+++ build/precache-manifest.52e4624f368d153bee1795006f8f4f51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83acf78b62a4e9b064fb7412bf7c147a",
+    "url": "/TeamApp/index.html"
+  },
+  {
+    "revision": "0bc295bef7c52b5e84f1",
+    "url": "/TeamApp/static/css/2.d88ce3aa.chunk.css"
+  },
+  {
+    "revision": "0bc295bef7c52b5e84f1",
+    "url": "/TeamApp/static/js/2.8468401a.chunk.js"
+  },
+  {
+    "revision": "472659a32763217e3642",
+    "url": "/TeamApp/static/js/main.af400f9e.chunk.js"
+  },
+  {
+    "revision": "53b62ca0a121b335b75e",
+    "url": "/TeamApp/static/js/runtime~main.ea5e8b48.js"
+  }
+]);
